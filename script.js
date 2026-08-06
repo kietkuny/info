1080e5134ebcb6072ec34e6380165aa2120613a9
Kiet Kun <dotuankiet00@gmail.com>
2026-08-06 19:37:31 +0700

    update

--- script.js
+++ script.js
@@ -45,48 +45,98 @@ $(document).ready(function () {
     ]
   });
 
-  $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    fade: true,
-    infinite: true,
-    asNavFor: '.slider-nav',
-    adaptiveHeight: false,
-    prevArrow: '<button type="button" class="slick-prev d-md-none custom-arrow prev" aria-label="Previous"></button>',
-    nextArrow: '<button type="button" class="slick-next d-md-none custom-arrow next" aria-label="Next"></button>'
-  });
+  // Helper để khởi tạo modal slider
+  function initModalSlider(modalId) {
+    const $modal = $('#' + modalId);
+    const $forSlider = $modal.find('.slider-for');
+    const $navSlider = $modal.find('.slider-nav');
 
-  $('.slider-nav').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    infinite: true,
-    dots: true,
-    centerMode: true,
-    centerPadding: '0px',
-    focusOnSelect: true,
-    arrows: true,
-    prevArrow: '<button type="button" class="slick-prev custom-arrow prev small-nav" aria-label="Previous"></button>',
-    nextArrow: '<button type="button" class="slick-next custom-arrow next small-nav" aria-label="Next"></button>',
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 998,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-    ]
-  });
+    // Unslick trước nếu đã khởi tạo
+    if ($forSlider.hasClass('slick-initialized')) {
+      $forSlider.slick('unslick');
+    }
+    if ($navSlider.hasClass('slick-initialized')) {
+      $navSlider.slick('unslick');
+    }
 
+    // Khởi tạo slider chính
+    $forSlider.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      fade: true,
+      infinite: true,
+      asNavFor: `#${modalId} .slider-nav`,
+      adaptiveHeight: false,
+      prevArrow: '<button type="button" class="slick-prev d-md-none custom-arrow prev" aria-label="Previous"></button>',
+      nextArrow: '<button type="button" class="slick-next d-md-none custom-arrow next" aria-label="Next"></button>'
+    });
+
+    // Khởi tạo slider nav
+    $navSlider.slick({
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      asNavFor: `#${modalId} .slider-for`,
+      infinite: true,
+      dots: true,
+      centerMode: true,
+      centerPadding: '0px',
+      focusOnSelect: true,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev custom-arrow prev small-nav" aria-label="Previous"></button>',
+      nextArrow: '<button type="button" class="slick-next custom-arrow next small-nav" aria-label="Next"></button>',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 5
+          }
+        },
+        {
+          breakpoint: 998,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+      ]
+    });
+
+    // Show slider-nav sau khi Slick khởi tạo
+    $navSlider.css('visibility', 'visible').css('opacity', '1');
+  }
+
+  // Xử lý modal banner
   $('#modal-banner').on('shown.bs.modal', function () {
-    $('.slider-for, .slider-nav').slick('setPosition');
+    initModalSlider('modal-banner');
+  });
+
+  $('#modal-banner').on('hidden.bs.modal', function () {
+    const $forSlider = $(this).find('.slider-for');
+    const $navSlider = $(this).find('.slider-nav');
+
+    if ($forSlider.hasClass('slick-initialized')) {
+      $forSlider.slick('unslick');
+    }
+    if ($navSlider.hasClass('slick-initialized')) {
+      $navSlider.slick('unslick');
+    }
+  });
+
+  // Xử lý modal img tương tự
+  $('#modal-img').on('shown.bs.modal', function () {
+    initModalSlider('modal-img');
+  });
+
+  $('#modal-img').on('hidden.bs.modal', function () {
+    const $forSlider = $(this).find('.slider-for');
+    const $navSlider = $(this).find('.slider-nav');
+
+    if ($forSlider.hasClass('slick-initialized')) {
+      $forSlider.slick('unslick');
+    }
+    if ($navSlider.hasClass('slick-initialized')) {
+      $navSlider.slick('unslick');
+    }
   });
 
   // Refresh AOS after slider initialization to recalculate offsets
